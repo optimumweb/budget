cbf31a16b466c6c1f4e98f7b681075473243f42d
Jonathan Roy <jroy@optimumweb.ca>
2012-07-16 14:13:14 -0400

    Style

--- budget.js
+++ budget.js
@@ -1,39 +1,39 @@
+function sectionAnnualTotal($section) {
+	
+	var total = 0;
+	
+	$section.find('.field').each(function() {
+		
+		var $this = $(this);
+		var $annualTotal = $this.find('.annual-total');
+		
+		total += parseFloat( $annualTotal.text() );
+		
+	});
+	
+	return total.toFixed(2);
+	
+}
+
+function calcSectionAnnualTotal($section) {
+	
+	$section.find('.field.total').find('.annual-total').text( '$' + sectionAnnualTotal( $section ) );
+	
+}
+
+function calcAnnualTotal($field) {
+	
+	var $amount = $field.find('.amount'), amount = parseFloat( $amount.find('input').val() );
+	var $period = $field.find('.period'), period = parseFloat( $period.find('select').val() );
+	var $annualTotal = $field.find('.annual-total');
+	
+	var annualTotal = amount * period;
+	
+	$annualTotal.text( '$' + annualTotal.toFixed(2) );
+	
+}
+
 $(document).ready(function() {
-	
-	function sectionAnnualTotal($section) {
-		
-		var total = 0;
-		
-		$section.find('.field').each(function() {
-			
-			var $this = $(this);
-			var $annualTotal = $this.find('.annual-total');
-			
-			total += parseFloat( $annualTotal.text() );
-			
-		});
-		
-		return total.toFixed(2);
-		
-	}
-	
-	function calcSectionAnnualTotal($section) {
-		
-		$section.find('.field.total').find('.annual-total').text( '$' + sectionAnnualTotal( $section ) );
-		
-	}
-	
-	function calcAnnualTotal($field) {
-		
-		var $amount = $field.find('.amount'), amount = parseFloat( $amount.find('input').val() );
-		var $period = $field.find('.period'), period = parseFloat( $period.find('select').val() );
-		var $annualTotal = $field.find('.annual-total');
-		
-		var annualTotal = amount * period;
-		
-		$annualTotal.text( '$' + annualTotal.toFixed(2) );
-		
-	}
 	
 	$('.field').each(function() {
 		
