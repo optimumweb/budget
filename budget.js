cc47d2f2619267c3014c9f05c0b87dd20c0d9d9c
Jonathan Roy <jroy@optimumweb.ca>
2012-07-16 17:39:10 -0400

    Style

--- budget.js
+++ budget.js
@@ -1,77 +1,52 @@
-var calcSection = function($section) {
-
-	var $fields = $section.find('.field');
-	var $total = $section.find('.total');
-	var $monthlyTotal = $total.find('.monthly-total');
+function refresh() {
 	
-	this.getMonthlyTotal = function() {
+	var $sections = $('.section');
+	
+	$sections.each(function() {
+	
+		var $section = $(this);
+		var $sectionTotal = $section.find('.total');
+		var $sectionMonthlyTotal = $sectionTotal.find('.monthly-total');
+		var $sectionFields = $section.find('.field');
 		
-		var total = 0;
+		var sectionMonthlyTotal = 0;
 		
-		$fields.each(function() {
+		$sectionFields.each(function() {
+		
 			var $field = $(this);
-			var $fieldMonthlyTotal = $field.find('.monthly-total');
-			total += parseFloat( $fieldMonthlyTotal.text() );
+			var $amount = $field.find('.amount'), amount = parseFloat( $amount.find('input').val() );
+			var $period = $field.find('.period'), period = parseFloat( $period.find('select').val() );
+			var $monthlyTotal = $field.find('.monthly-total'), monthlyTotal = parseFloat( $monthlyTotal.text() );
+			
+			// refresh field amount
+			$amount.find('input').val( amount.toFixed(2) );
+			
+			// refresh field monthly total
+			var newMonthlyTotal = amount * period;
+			$monthlyTotal.text( newMonthlyTotal.toFixed(2) );
+			monthlyTotal = newMonthlyTotal;
+			
+			// increment section monthly total
+			sectionMonthlyTotal += monthlyTotal;
+			
 		});
 		
-		return parseFloat( total );
+		// refresh section monthly total
+		$sectionMonthlyTotal.text( sectionMonthlyTotal.toFixed(2) );
 		
-	};
+		// refresh percentages
+		$sectionFields.each(function() {
+			
+			var $monthlyTotal = $field.find('.monthly-total'), monthlyTotal = parseFloat( $monthlyTotal.text() );
+			var $percentage = $field.find('.percentage'), percentage = parseFloat( $percentage.text() );
+			
+			var newPercentage = ( monthlyTotal / sectionMonthlyTotal ) * 100;
+			$percentage.text( newPercentage.toFixed(1) );
+			percentage = newPercentage;
+			
+		});
 	
-	this.calcMonthlyTotal = function() {
-		
-		var total = calcSection( $section ).getMonthlyTotal();
-		
-		$monthlyTotal.text( total.toFixed(2) );
-		
-		return this;
-		
-	};
-	
-	return $section;
-	
-};
-
-var calcField = function($field) {
-	
-	var $amount = $field.find('.amount'), amount = parseFloat( $amount.find('input').val() );
-	var $period = $field.find('.period'), period = parseFloat( $period.find('select').val() );
-	var $monthlyTotal = $field.find('.monthly-total'), monthlyTotal = parseFloat( $monthlyTotal.text() );
-	var $percentage = $field.find('.percentage'), percentage = parseFloat( $percentage.text() );
-
-	var $section = $field.parent('.section');
-	
-	this.refreshAmount = function() {
-		$amount.find('input').val( amount.toFixed(2) );
-		return this;
-	};
-	
-	this.calcMonthlyTotal = function() {
-		var newMonthlyTotal = amount * period;
-		$monthlyTotal.text( newMonthlyTotal.toFixed(2) );
-		monthlyTotal = newMonthlyTotal;
-		return this;
-	};
-	
-	this.calcPercentage = function() {
-		var sectionMonthlyTotal = calcSection( $section ).getMonthlyTotal();
-		var newPercentage = ( monthlyTotal / sectionMonthlyTotal ) * 100;
-		$percentage.text( newPercentage.toFixed(1) );
-		percentage = newPercentage;
-		return this;
-	};
-	
-	return $field;
-	
-};
-
-function calcAll($field) {
-	
-	$section = $field.parent('.section');
-	
-	calcField( $field ).refreshAmount().calcMonthlyTotal().calcPercentage();
-	
-	calcSection( $section ).calcMonthlyTotal();
+	});
 	
 }
 
@@ -80,20 +55,11 @@ $(document).ready(function() {
 	var $field = $('.field');
 	var $inputs = $field.find('input, select');
 	
-	$field.each(function() {
-	
-		var $this = $(this);
-		
-		calcAll( $field );
-		
-	});
+	refresh();
 	
 	$inputs.bind('change focusout blur', function() {
 		
-		var $this = $(this);
-		var $field = $this.parent('.field');
-		
-		calcAll( $field );
+		refresh();
 		
 	});
 
