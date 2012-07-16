4356c3d9f97f453ddaf04ce784d1006ffeee8834
Jonathan Roy <jroy@optimumweb.ca>
2012-07-16 14:29:43 -0400

    Style

--- budget.js
+++ budget.js
@@ -34,12 +34,6 @@ function calcAnnualTotal($field) {
 }
 
 $(document).ready(function() {
-
-	$('.field, .total, .category-label').hide();
-	
-	$('.section-label').click(function() {
-		$(this).parent('.section').find('.field, .total, .category-label').show();
-	});
 	
 	$('.field').each(function() {
 		
